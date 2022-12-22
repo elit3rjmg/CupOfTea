56ba547483def1b51a8737b9c524b29f6a6f0e9e
RJ <84078883+elit3rjmg@users.noreply.github.com>
2022-12-23 02:10:54 +0530

    Bug Fixes

--- v1.0/Cup of Tea v1.1.py
+++ v1.0/Cup of Tea v1.1.py
@@ -16,11 +16,14 @@ add = input('Step 1: ').lower()
 while add in rcps:    
     if add == 'water':
         cup.append(add)
+        
         add = input('Step 2: ').lower() 
         if add == 'boil':
+            
             add = input('Step 3: ').lower()
             if add == 'teabag':
                 cup.append(add)
+                
                 add = input('Step 4: ').lower()       
                 if add == 'sugar':
                     cup.append(add)
@@ -35,9 +38,11 @@ while add in rcps:
                 else: 
                     print('Item unavailable.')
                     continue                                            
+        
         else:
             print('Boil water first!')
             continue
+    
     else:
         print('Add water first!')
         continue
@@ -46,8 +51,8 @@ if cup == tea:
     print('Your Tea is ready')
 
 elif cup == milk_tea:
-
     print('Your Milk tea is ready.')
+    
 else:
     print('Your Tea(no sugar) is ready.')
 
